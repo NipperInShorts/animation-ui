cabacbf8a6b943b9e3f2c46a0c2a130c51450861
Justin Nipper <justin.nipper@me.com>
2023-02-10 19:09:29 -0600

    Animates chest in and out based on trigger enter/exit

---
+++ Assets/Scripts/IsometricAdventure/ChestBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBehavior : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            animator.SetBool("isOpen", true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("isOpen", false);
+        }
+    }
+}
